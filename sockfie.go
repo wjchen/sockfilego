b892e7ce8762b8fb9574a17c8b4cb98fed0e11f8
wenjin <chen@pc1.local>
2016-01-03 08:06:22 +0800

    fix

--- sockfie.go
+++ sockfie.go
@@ -69,7 +69,8 @@ func isCiaFile(path string) bool {
 		return false
 	}
 	ext := path[len(path)-4:]
-	if (ext[1] == 'c' || ext[1] == 'C') &&
+	if (ext[0] == '.') &&
+		(ext[1] == 'c' || ext[1] == 'C') &&
 		(ext[2] == 'i' || ext[2] == 'i') &&
 		(ext[3] == 'a' || ext[3] == 'A') {
 		return true
